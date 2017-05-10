65143e65b571210d2727b2df02a59bb7b93283ae
Adrian Biedrzycki <a@modules.io>
2017-05-09 21:09:36 -0700

    Add new buy api

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const http = require('http');
 const mkdirp = require('mkdirp');
 const rimraf = require('rimraf');
 const express = require('express');
+const bodyParser = require('body-parser');
+const bodyParserJson = bodyParser.json();
 const cookie = require('cookie');
 const rollup = require('rollup');
 const rollupPluginNodeResolve = require('rollup-plugin-node-resolve');
@@ -83,10 +85,13 @@ class AssetWallet {
           res.type('application/javascript');
           res.send(indexJs);
         });
-        app.get(path.join(prefix, '/api/status'), (req, res, next) => {
+        const cors = (req, res, next) => {
           res.set('Access-Control-Allow-Origin', req.get('Origin'));
           res.set('Access-Control-Allow-Credentials', true);
 
+          next();
+        };
+        const wordsParser = (req, res, next) => {
           const cookieHeader = req.get('Cookie');
 
           const _respondDefault = () => {
@@ -101,24 +106,65 @@ class AssetWallet {
             const words = c && c.words;
 
             if (words) {
-              const address = backendApi.getAddress(words);
+              req.words = words;
 
-              backendApi.requestLiveAssetBalances(address)
-                .then(assetSpecs => {
-                  res.json({
-                    address: address,
-                    assets: assetSpecs,
-                  });
-                })
-                .catch(err => {
-                  res.status(500);
-                  res.send(err.stack);
-                });
+              next();
             } else {
               _respondDefault();
             }
           } else {
             _respondDefault();
+          }
+        };
+        app.get(path.join(prefix, '/api/status'), cors, wordsParser, (req, res, next) => {
+          const {words} = req;
+          const address = backendApi.getAddress(words);
+
+          backendApi.requestLiveAssetBalances(address)
+            .then(assetSpecs => {
+              res.json({
+                address: address,
+                assets: assetSpecs,
+              });
+            })
+            .catch(err => {
+              res.status(500);
+              res.send(err.stack);
+            });
+        });
+        app.post(path.join(prefix, '/api/buy'), cors, wordsParser, bodyParserJson, (req, res, next) => {
+          const {words, body} = req;
+
+          if (
+            typeof body == 'object' && body &&
+            typeof body.srcAsset === 'string' &&
+            typeof body.srcQuantity === 'number' &&
+            typeof body.dstAsset === 'string' &&
+            typeof body.dstQuantity === 'number'
+          ) {
+            const {srcAsset, srcQuantity, dstAsset, dstQuantity} = body;
+            const address = backendApi.getAddress(words);
+            const wifKey = backendApi.getKey(words);
+            const expiration = 1;
+
+            backendApi.requestCreateOrder(address, srcAsset, srcQuantity, dstAsset, dstQuantity, expiration, wifKey, {immediate: true})
+              .then(txid => {
+                res.json({
+                  txid,
+                });
+              })
+              .catch(err => {
+                if (err.code === 'EIMMEDIATE') {
+                  res.status(412);
+                  res.send();
+                } else {
+                  res.status(500);
+                  res.send(err.stack);
+                }
+              });
+          } else {
+            res.status(400);
+            res.send();
           }
         });
         app.use(path.join(prefix, '/'), express.static(path.join(__dirname, 'public')));
